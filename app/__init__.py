83d2267befe67fc9c2d80764470b85cfc53fb12c
mayreeh <shirrohmary@gmail.com>
2016-06-21 13:35:42 +0300

    Database Migrations added

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask  
 from flask_sqlalchemy import SQLAlchemy 
 from flask_script import Manager
+from config import Config
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+db.create_all()
 
 from views.home import home
 app.register_blueprint(home)
